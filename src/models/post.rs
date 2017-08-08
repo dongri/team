c23b88e67871f4226a9341abdb7ab4f6b11d2e45
Dongri Jin <dongrify@gmail.com>
2017-08-08 14:47:19 +0900

    Check len

--- src/models/post.rs
+++ src/models/post.rs
@@ -214,13 +214,16 @@ pub fn get_feeds(conn: &db::PostgresConnection, offset: &i32, limit: &i32) -> Re
         order by created desc offset $1::int limit $2::int", &[&offset, &limit]).unwrap() {
         match models::tag::get_tags_by_post_id(&conn, &row.get("id")) {
             Ok(tags) => {
-                let body: String = row.get("body");
+                let mut body: String = row.get("body");
+                if body.len() > 100 {
+                    body = body.as_str()[0..100].to_string()
+                }
                 feeds.push(Feed {
                     id: row.get("id"),
                     kind: row.get("kind"),
                     user_id: row.get("user_id"),
                     title: row.get("title"),
-                    body: body.as_str()[0..100].to_string(),
+                    body: body,
                     created: row.get("created"),
                     user: models::user::User{
                         id: row.get("user_id"),
